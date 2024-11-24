1713b588cac12d50a86672b14c23c32c3df3b970
mohanrajsm <mohan_raaaj@yahoo.co.in>
2024-11-24 17:04:10 +0530

    Update NewProjectApplication.java

--- src/main/java/com/mohan/newproject/NewProjectApplication.java
+++ src/main/java/com/mohan/newproject/NewProjectApplication.java
@@ -9,6 +9,7 @@ public class NewProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewProjectApplication.class, args);
 		System.out.println("Hello from Goggle Git");
+		System.out.println("Hello from Yahoo Git");
 	}
 
 }
